07374e93aa69eb02ce0c52311473905adb984d18
maxmalysh <maxmalysh@gmail.com>
2016-01-07 21:49:21 +0300

    Input reading & pretty-printing

---
+++ alignment.py
@@ -0,0 +1,6 @@
+
+def needleman(seq1, seq2, affine=False):
+    return seq1
+
+def smith(seq1, seq2, affine=False):
+    return seq1
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from alignment import needleman, smith
 
 #   
 # Parse input arguments
@@ -21,4 +22,46 @@ if args.needleman == args.smith:
 if args.filename == None:
     print("You should provide a file with two sequences to be aligned")
     exit(-1)
+
+#
+# Read an input file
+#
+seq = []
+with open(args.filename, 'r') as f:
+    for line in f:
+        line.strip()
+        n = -1
+
+        if line.startswith("> seq"):
+            n += 1
+            seq.append('')
+            continue
+
+        if line:
+            seq[n] += line.strip().replace(' ', '')
+
+#
+# Perform the alignment
+#
+align = needleman if args.needleman else (smith if args.smith else None)
+aligned = align(seq[0], seq[1])
+
+#
+# Print output
+#
+if len(seq[0]) < 20 and len(seq[1]) < 20:
+    # Pretty-printing short sequences as in Wikipedia article
+    extraws = max(0, len(seq[1])-len(seq[0])), max(0, len(seq[0]) - len(seq[1]))
+    extraws = [' ' * (ws+4) for ws in extraws]
+    spaces  = ' ' * len(max(extraws[0], extraws[1]))
+
+    print("Sequences%sAligned Sequences" % spaces)
+    print("---------%s-----------------" % spaces)
+    print("%s %s %s" % (seq[0][0:20], extraws[0], aligned[0][0:20]))
+    print("%s %s %s" % (seq[1][0:20], extraws[1], aligned[1][0:20]))
+else:
+    print(aligned[0], '\n')
+    print(aligned[1])
+
+
 
